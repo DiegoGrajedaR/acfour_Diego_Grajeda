2aef832163dccb3fe526a152826773bbdbba0bfa
DiegoGrajedaR <diego.grajeda.7e8@itb.cat>
2024-12-10 20:56:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace acfour_Diego_Grajeda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
