f338e6165fa152519f0b47587baa2d77e27073c2
DiegoGrajedaR <diego.grajeda.7e8@itb.cat>
2024-12-14 19:16:37 +0100

    Implementació feta del diagrama de flux a C#

--- Program.cs
+++ Program.cs
@@ -5,7 +5,22 @@ namespace acfour_Diego_Grajeda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Introdueix un número per establir el tamany de l'array: ");
+            int arraySize = int.Parse(Console.ReadLine());
+
+            int[] arrayNums = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"Introdueix el nombre {i + 1}: ");
+                arrayNums[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Els nombres introduïts són:");
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.WriteLine(arrayNums[i]);
+            }
         }
     }
 }
